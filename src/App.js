c06f790db241d190d4a6d621d3bc890b1ea2c71a
xJkit-iMac <xjkit54780@gmail.com>
2017-01-04 01:24:10 +0800

    Setup firebase config

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+
+  componentWillMount() {
+    // Initialize Firebase
+    const config = {
+      apiKey: 'AIzaSyCc_MdukrWJEF1UwtT5buPV1mi5ttZVrS8',
+      authDomain: 'auth-6c370.firebaseapp.com',
+      databaseURL: 'https://auth-6c370.firebaseio.com',
+      storageBucket: 'auth-6c370.appspot.com',
+      messagingSenderId: '612097248292'
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <View>
